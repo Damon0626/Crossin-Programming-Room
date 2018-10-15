922ce18e71e01fbe82d68ea4cd2c7dd22cc8e8b5
Damon <33386246+Damon0626@users.noreply.github.com>
2018-10-15 23:48:18 +0800

    Update readme.m

--- 06-Personal tax calculator1015/readme.m
+++ 06-Personal tax calculator1015/readme.m
@@ -1,1 +1,14 @@
+ # 个税计算器
 
+之前用Tkinter写过一个贷款计算器，还真是丑。最近正好Crossin教室老师有个个税计算器的小作业，利用业余时间，学习PyQt5，写了个税计算器，相比Tkinter
+好看多了。2333~
+
+实现了以下几个功能：
+1.输入内容要求为数字，且数字范围在[0, 99999999];
+2.添加了一个超链接；
+3.如果发生错误，进行相应的弹窗；
+4.设置重置功能。
+
+![pic1](https://github.com/Damon0626/Crossin-Programming-Room/blob/master/06-Personal%20tax%20calculator1015/pic1.png)
+![pic2](https://github.com/Damon0626/Crossin-Programming-Room/blob/master/06-Personal%20tax%20calculator1015/pic2.png)
+![pic3](https://github.com/Damon0626/Crossin-Programming-Room/blob/master/06-Personal%20tax%20calculator1015/pic3.png)
